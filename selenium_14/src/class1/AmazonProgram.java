c804c58da6fd6551284ce9df8f4b6bbda75d1c43
DELL <gcreddy1997@gmail.com>
2020-06-12 07:33:06 +0530

    Some additions and deletions

--- selenium_14/src/class1/AmazonProgram.java
+++ selenium_14/src/class1/AmazonProgram.java
@@ -19,7 +19,7 @@ public class AmazonProgram {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-		
+		// some comments added in line number 22
 		//invoking the URL
 		driver.get("https://www.amazon.com/");
 		
@@ -35,20 +35,17 @@ public class AmazonProgram {
 		
 		//select baby checkbox
 		driver.findElement(By.xpath("(//*[@class='a-label a-checkbox-label'])[3]")).click();
-		
 		//make list and store 32 elements
 		l = driver.findElements(By.xpath("//*[@class='a-row dealContainer']"));
 		System.out.println("size of list" +l.size() );
-		
-		//select 2 element
+			//select 2 element
 		l.get(1).click();
-		
-		//select last element of 2 element
+			//select last element of 2 element
 		l = driver.findElements(By.xpath("//*[@class='a-section aok-relative s-image-fixed-height']"));
 		System.out.println("size of list" +l.size() );
-		
 		//select last element
 		l.get(l.size()-1).click();
+		// just for checking in line number 52
 	
 		
 	
